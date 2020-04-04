c362d986e0f7de3768b16b9eefac15e660189d2f
Stephen O'Connor <stephenoconnor01@googlemail.com>
2020-04-04 09:44:44 +0100

    Added eslint-a11y settings.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const Warning = React.lazy(() => import('./Warning'));
 
 class App extends Component {
 	state = {
-		count: 0,
+		count: 0
 	};
 
 	render() {
@@ -15,8 +15,8 @@ class App extends Component {
 			<div>
 				<h3>React App</h3>
 				<h4 className={count > 10 ? 'warning' : null}>Count: {count}</h4>
-				<button onClick={() => this.setState((state) => ({ count: state.count + 1 }))}>+</button>
-				<button onClick={() => this.setState((state) => ({ count: state.count - 1 }))}>-</button>
+				<button onClick={() => this.setState(state => ({ count: state.count + 1 }))}>+</button>
+				<button onClick={() => this.setState(state => ({ count: state.count - 1 }))}>-</button>
 				{count > 10 ? (
 					<Suspense fallback={null}>
 						<Warning />

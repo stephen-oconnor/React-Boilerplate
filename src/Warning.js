a5cccb6f84cbd28954a67edae6e73c62571543b2
Stephen O'Connor <stephenoconnor01@googlemail.com>
2020-04-04 10:58:42 +0100

    Code cleanup.

--- src/Warning.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './styles.css';
-
-const Warning = React.memo(function Warning() {
-	return <span className={'warning'}>Take it easy!</span>;
-});
-
-export default Warning;
